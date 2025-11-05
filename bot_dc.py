c277abdca296e1f6df09992c22b7bd3a035f6200
Wojcio-tech <gawryluk.wojtek5@gmail.com>
2025-11-05 19:26:22 +0100

    Implement basic Discord bot with commands

---
+++ bot_dc.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh: int = 5):  
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+bot.run("TOKEN")
